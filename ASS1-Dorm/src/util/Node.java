e3d479ffb057000ba968fb7400ea077701019baa
Minh3119 <65076982+Minh3119@users.noreply.github.com>
2024-10-02 23:56:40 +0700

    changed structure

---
+++ ASS1-Dorm/src/util/Node.java
@@ -0,0 +1,21 @@
+/**
+ *
+ * @author Admin
+ */
+package util;
+
+public class Node {
+    int info;
+    Node next;
+
+    public Node() {}
+
+    public Node(int info, Node next) {
+        this.info = info;
+        this.next = next;
+    }
+
+    public Node(int info) {
+        this(info, null);
+    }
+}
