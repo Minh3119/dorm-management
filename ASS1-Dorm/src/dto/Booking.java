a78b2389fb919468bada091736eb3b1be0e6a549
Minh3119 <65076982+Minh3119@users.noreply.github.com>
2024-10-02 22:32:27 +0700

    added constructor and get/setters for Booking

--- ASS1-Dorm/src/dto/Booking.java
+++ ASS1-Dorm/src/dto/Booking.java
@@ -5,6 +5,55 @@
 
 package dto;
 
+import java.time.LocalDate;
+
 public class Booking {
+    
+    private String rcode;
+    private String scode;
+    private LocalDate bookDate;
+    private LocalDate leaveDate;
+    private int state;      // 0 or 1 only
+
+    public Booking(String rcode, String scode, LocalDate bookDate, LocalDate leaveDate, int state) {
+        this.rcode = rcode;
+        this.scode = scode;
+        this.bookDate = bookDate;
+        this.leaveDate = leaveDate;
+        this.state = state;
+    }
+
+    public String getRcode() {
+        return rcode;
+    }
+
+    public String getScode() {
+        return scode;
+    }
+
+    public LocalDate getBookDate() {
+        return bookDate;
+    }
+
+    public LocalDate getLeaveDate() {
+        return leaveDate;
+    }
+
+    public int getState() {
+        return state;
+    }
+
+    public void setBookDate(LocalDate bookDate) {
+        this.bookDate = bookDate;
+    }
+
+    public void setLeaveDate(LocalDate leaveDate) {
+        this.leaveDate = leaveDate;
+    }
+
+    public void setState(int state) {
+        this.state = state;
+    }
+    
     
 }
