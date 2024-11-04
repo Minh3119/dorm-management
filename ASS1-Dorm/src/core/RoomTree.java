1a5dda3b4d17a0e59ba078ede879e04fd437108d
Minh3119 <65076982+Minh3119@users.noreply.github.com>
2024-11-04 09:57:18 +0700

    adding methods foro RoomTree (uncompleted)

--- ASS1-Dorm/src/core/RoomTree.java
+++ ASS1-Dorm/src/core/RoomTree.java
@@ -3,7 +3,14 @@ package core;
 import base.MyQueue;
 import base.TreeNode;
 import dto.Room;
+import dto.RoomType;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
+import util.Inputter;
 
 
 public class RoomTree {
@@ -465,4 +472,173 @@ public class RoomTree {
             }
         }
     }
+    
+    // 1.1
+    public int loadData(String filename) {
+        // data = rcode, name, dom, floor, type, booked, price
+        int count = 0;
+        try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] data = line.split(",");
+                if (data.length == 7) {
+                    RoomType type = Inputter.convertsToRoomType(data[4]);
+                    int booked = Integer.parseInt(data[5]);
+                    double price = Double.parseDouble(data[6]);
+
+                    Room room = new Room(data[0], data[1], data[2], data[3], type, booked, price);
+                    if (searchByCode(data[0]) == null) {
+                        this.insert(room);
+                        count++;
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return count;
+    }
+
+    // 1.2
+    public void addToEnd(Room room) {
+        insert(room);
+    }
+
+    // 1.3
+    public void display() {
+        // display all rooms in the list
+        System.out.println("-------------------------------------------------------------------------------------------------");
+        System.out.format("%-10s | %-20s | %-5s | %-5s | %-7s | %4s | %-6s | %s\n",
+                "rcode", "name", "dom", "floor", "type", "beds", "booked", "price");
+        System.out.println("-------------------------------------------------------------------------------------------------");
+        inOrder(this.root);
+    }
+
+    public void display(Room room) {
+        // display all rooms in the list
+//        System.out.println("-------------------------------------------------------------------------------------------------");
+        System.out.format("%-10s | %-20s | %-5s | %-5s | %-7s | %4s | %-6s | %s\n",
+                "rcode", "name", "dom", "floor", "type", "beds", "booked", "price");
+        System.out.println("-------------------------------------------------------------------------------------------------");
+        System.out.println(room);
+    }
+
+    // 1.4
+    public void saveData(String filename) {
+        // data = rcode, name, dom, floor, type, booked, price
+        loadData(filename);
+
+        if (this.isEmpty()) {
+            System.out.println("No rooms found.");
+            return;
+        }
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filename, false))) {
+            saveDataPostOrder(this.root, writer);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.format("Saved rooms to %s \n", filename);
+    }
+    
+    public void saveDataPostOrder(TreeNode<Room> p, BufferedWriter writer) throws IOException {
+        if (p == null) {
+            return;
+        }
+        saveDataPostOrder(p.left, writer);
+        saveDataPostOrder(p.right, writer);
+        
+        String[] lineComponents = new String[7];
+        Room room = p.info;
+        lineComponents[0] = room.getRcode();
+        lineComponents[1] = room.getName();
+        lineComponents[2] = room.getDom();
+        lineComponents[3] = room.getFloor();
+        lineComponents[4] = room.getRoomType().toString();
+        lineComponents[5] = String.valueOf(room.getBooked());
+        lineComponents[6] = String.valueOf(room.getPrice());
+        String line = String.join(",", lineComponents);
+        writer.write(line);
+        writer.newLine();
+    }
+    
+    // 1.5
+    public Room searchByCode(String rcode) {
+        TreeNode<Room> p = root;
+        while (p != null) {
+            if (p.info.getRcode().equals(rcode)) {
+                return p.info;
+            }
+            p = p.getNext();
+        }
+        return null;
+    }
+    
+    // 1.6
+    public void deleteByCode(String rcode) {
+        TreeNode<Room> p = searchNodeByCode(rcode);
+        if (p != null) {
+            this.remove(p);
+            System.out.println("Room with code " + rcode + " has been deleted.");
+        } else {
+            System.out.println("Room with code " + rcode + " not found.");
+        }
+    }
+
+    // 1.7
+    public void sortByCode() {
+        // sort the list based on rcode ASCENDING
+        TreeNode<Room> p = head;
+        TreeNode<Room> q;
+        while (p != null) {
+            q = p.getNext();
+            while (q != null) {
+                if (p.getInfo().getRcode().compareTo(q.getInfo().getRcode()) > 0) {
+                    swap(p, q);
+                }
+                q = q.getNext();
+            }
+            p = p.getNext();
+        }
+    }
+
+    // 1.8
+    public void addToBeginning(Room room) {
+        addFirst(room);
+    }
+
+    // 1.9
+    public void addBeforeIndex(int k, Room room) {
+        insert(k, room);
+    }
+
+    // 1.10 
+    public void deleteByIndex(int index) {
+        this.remove(index);
+    }
+
+    // 1.11
+    public void searchByName(String name) {
+        TreeNode<Room> current = head;
+        TreeNode<Room> firstMatch = null;  // Lưu node đầu tiên tìm thấy
+
+        while (current != null) {   //pressing nguyên sàn 
+            if (current.getInfo().getName().equals(name)) {
+                if (firstMatch == null) {
+                    display(current.getInfo());
+                    firstMatch = current;
+                } else {
+                    System.out.println(current.getInfo());
+                }
+            }
+            current = current.getNext();
+        }
+
+        if (firstMatch == null) {
+            System.out.println("No rooms found with name: " + name);
+        }
+
+    }
+    
+    // 1.12
 }
