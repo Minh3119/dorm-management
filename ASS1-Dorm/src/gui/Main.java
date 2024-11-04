1b1b3f3e902c632a957a3cf9088db975fc475534
AnhDucNgo1245 <160697607+AnhDucNgo1245@users.noreply.github.com>
2024-11-04 13:25:17 +0700

    Update main, ...
    gg

--- ASS1-Dorm/src/gui/Main.java
+++ ASS1-Dorm/src/gui/Main.java
@@ -6,79 +6,79 @@ package gui;
 
 import core.BookingList;
 import core.RoomTree;
+import core.StudentTree;
 import dto.Room;
+import dto.Student;
 import dto.RoomType;
 import java.util.Arrays;
 import util.Inputter;
 import util.Menu;
 
 public class Main {
-    
+
     private static String[] OPTIONS = {
         "1. Room management",
         "2. Student management",
         "3. Booking management",
         "0. Exit"
     };
-    
+
     private static String[] ROOM_OPTIONS = {
         "1. Load data from file",
-        "2. Input & add to the end",
-        "3. Display room data",
-        "4. Save room list to file",
-        "5. Search by room code",
-        "6. Delete by room code",
-        "7. Sort by room code",
-        "8. Input & add to beginning",
-        "9. Add before position k",
-        "10. Delete position k",
-        "11. Search by room name",
-        "12. Search booked room by room code",
+        "2. Input & add to the tree",
+        "3. Display data by in-order traversal",
+        "4. Save room tree to file by post-order traversal",
+        "5. Search room by rcode",
+        "6. Delete by bcode by copying",
+        "7. Delete by bcode by merging",
+        "8. Simply balancing",
+        "9. Display data by breadth-first traversal",
+        "10. Count the number of rooms",
+        "11. Search by name",
+        "12. Search booked by rcode",
         "0. Go back"
     };
-    
+
     private static String[] STUDENT_OPTIONS = {
         "1. Load data from file",
-        "2. Input & add to the end",
-        "3. Display student data",
-        "4. Save student list to file",
-        "5. Search by student code",
-        "6. Delete by student code",
+        "2. Input & add to the tree",
+        "3. Display data by pre-order traversal",
+        "4. Save student tree to file by in-order traversal",
+        "5. Search by scode",
+        "6. Delete by scode by copying",
         "7. Search by student name",
-        "8. Search booked room by student code",
+        "8. Search booking room by scode",
         "0. Go back"
     };
-    
+
     private static String[] BOOKING_OPTIONS = {
         "1. Load data from file",
         "2. Book a room",
         "3. Display booking data",
         "4. Save booking list to file",
-        "5. Sort",
-        "6. Leave a room",
+        "5. Sort by rcode + scode",
+        "6. Leave a room by rcode + scode",
         "0. Go back"
     };
-    
-    
+
     private final static BookingList bookingList = new BookingList();
-//    private final static StudentList studentList = new StudentList();
+    private final static StudentTree studentTree = new StudentTree();
     private final static RoomTree roomTree = new RoomTree();
 
-    
     public static void main(String[] args) {
         displayMainMenu();
     }
-    
+
     // Works fine for now
     static void displayMainMenu() {
         Menu menu = new Menu("Dorm Management Program", "Your choice: ");
         menu.addAll(Arrays.asList(OPTIONS));
-        
+
         while (true) {
             menu.display();
-            
+
             int choice;
-            choice = Inputter.getInt(0,OPTIONS.length);
+            choice = Inputter.getInt(0, OPTIONS.length);
             switch (choice) {
                 case 0:
                     return;
@@ -91,20 +91,20 @@ public class Main {
                 case 3:
                     displayMenuBooking();
                     break;
-            }            
+            }
         }
     }
-    
+
     // need implementations
     static void displayMenuRoom() {
         Menu menu = new Menu("Room options", "Your choice: ");
         menu.addAll(Arrays.asList(ROOM_OPTIONS));
-        
+
         while (true) {
             menu.display();
 
             int choice;
-            choice = Inputter.getInt(0,ROOM_OPTIONS.length);
+            choice = Inputter.getInt(0, ROOM_OPTIONS.length);
             System.out.println("");
             String rcode, rname, dom, floor;
             RoomType roomType;
@@ -176,7 +176,7 @@ public class Main {
                     } else {
                         System.out.println("Room with code " + rcode + " not found.");
                     }
-                    
+
                     break;
                 case 6:
                     System.out.print("Input room code: ");
@@ -200,7 +200,7 @@ public class Main {
                     System.out.println("Found " + count + " rooms.");
                     break;
                 case 11:
-                    System.out.print("Input room code: ");
+                    System.out.print("Input room name: ");
                     rname = Inputter.getString();
                     room = roomTree.searchByName(rname);
 
@@ -217,83 +217,140 @@ public class Main {
             }
         }
     }
-    
+
     // need implementations
     static void displayMenuStudent() {
         Menu menu = new Menu("Student options", "Your choice: ");
         menu.addAll(Arrays.asList(STUDENT_OPTIONS));
-        
+
         while (true) {
             menu.display();
 
             int choice;
-            choice = Inputter.getInt(0,STUDENT_OPTIONS.length);
+            choice = Inputter.getInt(0, STUDENT_OPTIONS.length);
             System.out.println("");
             switch (choice) {
                 case 0:
                     return;
                 case 1:
-                    studentManager.loadData();
+                    studentTree.loadData("students.txt");
                     break;
                 case 2:
-                    studentManager.createNewStudentAtEnd();
+                    System.out.println("Please insert infomation for the new room:");
+
+                    // get scode
+                    System.out.print("Student's Code: ");
+                    while (true) {
+                        String scode = Inputter.getString();
+                        if (studentTree.searchByCode(scode) == null) {
+                            break;
+                        } else {
+                            System.out.print("Student with code " + scode + " already exists. Try again: ");
+                        }
+                    }
+
+                    // get sname
+                    String sname = Inputter.getName("Student's Name: ");
+
+                    // get bYear
+                    System.out.print("Student's BirthYear: ");
+                    int bYear = Inputter.checkBirthYear();
+
+                    // get booked Room
+                    //....
+                    Student newStudent = new Student(sname, sname, bYear);
+                    studentTree.addToEnd(newStudent);
                     break;
-                case 3:
-                    studentManager.display();
+                case 3: //Display data by pre-order traversal
+                    studentTree.displayByPreOrder();
                     break;
-                case 4:
-                    studentManager.saveData();
+                case 4: // Save Data in-order
+                    studentTree.saveData("students.txt");
                     break;
-                case 5:
-                    studentManager.searchStudentByCode();
+                case 5: // search by scode
+                    System.out.print("Input Student code: ");
+                    String scode = Inputter.getString();
+                    Student s = studentTree.searchByCode(scode);
+
+                    if (s != null) {
+                        System.out.println("Student found: ");
+                        studentTree.searchByCode(scode);
+                    } else {
+                        System.out.println("Student with code " + scode + " not found.");
+                    }
                     break;
                 case 6:
-                    studentManager.deleteStudentByCode();
+                    System.out.print("Input Student code: ");
+                    scode = Inputter.getString();
+                    studentTree.deleteByCopying(studentTree.searchByCode(scode));
                     break;
                 case 7:
-                    studentManager.searchStudentByName();
+                    System.out.print("Input student name: ");
+                    sname = Inputter.getString();
+                    studentTree.searchByName(sname);
+
                     break;
                 case 8:
-                    studentManager.searchBookedRoomByStudentCode(bookingList);
+                    System.out.print("Input Student code: ");
+                    scode = Inputter.getString();
+                    studentTree.searchStudentRoom(scode);
                     break;
             }
         }
     }
-    
+
     // need implementations
     static void displayMenuBooking() {
         Menu menu = new Menu("Booking options", "Your choice: ");
         menu.addAll(Arrays.asList(BOOKING_OPTIONS));
-        
+
         while (true) {
             menu.display();
 
             int choice;
-            choice = Inputter.getInt(0,BOOKING_OPTIONS.length);
+            choice = Inputter.getInt(0, BOOKING_OPTIONS.length);
             System.out.println("");
             switch (choice) {
                 case 0:
                     return;
                 case 1:
-                    bookingManager.loadData();
+                    bookingList.loadData("booking.txt");
                     break;
                 case 2:
-                    bookingManager.bookRoom();
-                    break;
+                    System.out.println("Please insert infomation for the new booking: ");
+
+                    // get rcode
+                    System.out.print("Room Code: ");
+                    String rcode = Inputter.getString();
+                    //.....
+
+                    // get sname
+                    String scode = Inputter.getName("Student Code ");
+
+                    //....
                 case 3:
-                    bookingManager.display();
+                    bookingList.display();
                     break;
                 case 4:
-                    bookingManager.saveData();
+                    bookingList.saveData("booking.txt");
                     break;
-                case 5:
-                    bookingManager.sort();
+                case 5: // HÀM SAI
+                    bookingList.sort();
                     break;
                 case 6:
-                    bookingManager.leaveRoom();
+                    // get rcode
+                    System.out.print("Room Code: ");
+                    rcode = Inputter.getString();
+                    //.....
+
+                    // get sname
+                    scode = Inputter.getName("Student Code ");
+                    
+                    
+                    bookingList.leaveRoom(rcode, scode, roomTree);
                     break;
             }
         }
     }
-    
+
 }
--- ASS1-Dorm/src/util/Inputter.java
+++ ASS1-Dorm/src/util/Inputter.java
@@ -2,17 +2,17 @@
  *
  * @author Group 4
  */
-
 package util;
 
 import java.util.Scanner;
 import util.Menu;
 import dto.RoomType;
+import java.util.Calendar;
 
 public class Inputter {
-    
+
     private static Scanner scanner = new Scanner(System.in);
-    
+
     public static RoomType convertsToRoomType(String s) {
         if (s.compareTo("DOUBLE") == 0) {
             return RoomType.DOUBLE;
@@ -21,7 +21,7 @@ public class Inputter {
         }
         return null;
     }
-    
+
     public static float getPrice() {
         while (true) {
             String s = scanner.nextLine();
@@ -36,13 +36,12 @@ public class Inputter {
             }
         }
     }
-    
+
 //    public static int getBookedBeds(RoomType roomType) {
 //        
 //        int maxBeds = roomType.getBeds();
 //        return getInt(0, maxBeds);
 //    }
-    
     public static String getString() {
         String s;
         while (true) {
@@ -55,23 +54,23 @@ public class Inputter {
         }
         return s;
     }
-    
+
     public static RoomType getRoomType() {
         Menu menu = new Menu("There are 2 types of room:", "Your choice: ");
-        menu.add("1. Double room (" +RoomType.DOUBLE.getBeds()+ " beds)");
-        menu.add("2. Triple room (" +RoomType.TRIPLE.getBeds()+ " beds)");
+        menu.add("1. Double room (" + RoomType.DOUBLE.getBeds() + " beds)");
+        menu.add("2. Triple room (" + RoomType.TRIPLE.getBeds() + " beds)");
         menu.display();
 
-        int choice = getInt(1,2);
+        int choice = getInt(1, 2);
         switch (choice) {
             case 1:
-                return RoomType.DOUBLE;                
+                return RoomType.DOUBLE;
             case 2:
                 return RoomType.TRIPLE;
         }
         return null;
     }
-    
+
     public static int getInt(int min, int max) {
         while (true) {
             String s = scanner.nextLine();
@@ -86,8 +85,48 @@ public class Inputter {
             }
         }
     }
-    
+
     private boolean isValidBeds(int beds, RoomType roomType) {
         return beds <= roomType.getBeds();
+    }
+
+    public static String getName(String m) {
+        System.out.print(m);
+        while (true) {
+            String s = scanner.nextLine();
+            if (isValidName(s)) {
+                return s;
+            }
+            return null;
+        }
+    }
+
+    public static boolean isValidName(String name) {
+        if (name.trim().isEmpty()) {
+            return false;
+        }
+
+        String[] nameParts = name.split(" ");
+
+        for (String s : nameParts) {
+            if (!s.matches("[a-zA-z]+") || !Character.isUpperCase(s.charAt(0))) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    public static int checkInputExprience(int birthDate) {
+        int yearCurrent = Calendar.getInstance().get(Calendar.YEAR);
+        int age = yearCurrent - birthDate;
+        while (true) {
+            int yearExperience = getInt(1900, yearCurrent);
+            if (age < 18) {
+                System.err.println("Birth year must greater than 18 !!! ");
+            } else {
+                return yearExperience;
+            }
+        }
+
     }
 }
